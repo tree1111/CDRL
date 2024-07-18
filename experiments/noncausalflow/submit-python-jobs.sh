066b63aa57e99c113f3c7882f713059764866dde
Adam Li <adam2392@gmail.com>
2024-07-18 15:40:11 -0400

    Running non causal nf on gpu 1
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/noncausalflow/submit-python-jobs.sh
+++ experiments/noncausalflow/submit-python-jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory containing the Python script
-SCRIPT_NAME="02_train_realnvp_model.py"
+SCRIPT_NAME="01_train_noncausalnf.py"
 # SCRIPT_NAME="03_train_realnvp_model_ind_noise.py"
 
 LOG_DIR="/home/adam2392/projects/logs/"
