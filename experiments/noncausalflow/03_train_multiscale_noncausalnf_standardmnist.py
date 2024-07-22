fbd0204b5f2356aa3efef57665fa08c85ea88990
Adam Li <adam2392@gmail.com>
2024-07-22 15:22:42 -0400

    Run normal MNIST and see if it works without dequantization
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/noncausalflow/03_train_multiscale_noncausalnf_standardmnist.py
+++ experiments/noncausalflow/03_train_multiscale_noncausalnf_standardmnist.py
@@ -42,9 +42,9 @@ class MNISTDataModule(pl.LightningDataModule):
         self.transform=transform
 
     def setup(self, stage: str):
-        self.mnist_test = MNIST(self.data_dir, train=False)
-        self.mnist_predict = MNIST(self.data_dir, train=False)
-        mnist_full = MNIST(self.data_dir, train=True)
+        self.mnist_test = MNIST(self.data_dir, download=True, train=False)
+        self.mnist_predict = MNIST(self.data_dir, download=True, train=False)
+        mnist_full = MNIST(self.data_dir, download=True, train=True)
         self.mnist_train, self.mnist_val = random_split(
             mnist_full, [55000, 5000], generator=torch.Generator().manual_seed(42)
         )
@@ -99,11 +99,11 @@ if __name__ == "__main__":
     lr_min = 1e-7
     lr = 1e-3
 
-    root = "/Users/adam2392/pytorch_data/"
-    accelerator = "cpu"
-    intervention_types = [None, 1]
-    num_workers = 1
-    batch_size = 10
+    # root = "/Users/adam2392/pytorch_data/"
+    # accelerator = "cpu"
+    # intervention_types = [None, 1]
+    # num_workers = 1
+    # batch_size = 10
     print(args)
     # root = args.root_dir
     seed = args.seed
