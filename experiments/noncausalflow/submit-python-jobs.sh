7f92a746180ce846836533d0454b2d57ea298825
Adam Li <adam2392@gmail.com>
2024-07-21 21:29:26 -0400

    Train 3.1 M model
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/noncausalflow/submit-python-jobs.sh
+++ experiments/noncausalflow/submit-python-jobs.sh
@@ -36,7 +36,7 @@ do
   
   # Optionally, you can use a job scheduler like `nohup` to run the command in the background
   # or `&` to run the command in the background
-  nohup $CMD > output_multiscalenf_128batch_${SCRIPT_NAME}_seed_${TRAINING_SEED}.log 2>&1 &
+  nohup $CMD > output_multiscalenf_256batch_${SCRIPT_NAME}_seed_${TRAINING_SEED}.log 2>&1 &
 
   echo $TRAINING_SEED
   echo "GPU index is $CUDA_VISIBLE_DEVICES"
