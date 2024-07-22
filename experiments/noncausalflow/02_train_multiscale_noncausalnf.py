66ac0c3bec19ea105827b30695333bf3f24dce8a
Adam Li <adam2392@gmail.com>
2024-07-21 21:29:15 -0400

    Train 3.1 M model
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/noncausalflow/02_train_multiscale_noncausalnf.py
+++ experiments/noncausalflow/02_train_multiscale_noncausalnf.py
@@ -40,11 +40,11 @@ def add_main_args(parser):
     # training misc args
     parser.add_argument("--root_dir", type=str, default="./", help="Root directory")
     parser.add_argument("--seed", type=int, default=1234, help="random seed")
-    parser.add_argument("--max_epochs", type=int, default=10_000, help="Max epochs")
+    parser.add_argument("--max_epochs", type=int, default=20_000, help="Max epochs")
     parser.add_argument(
         "--accelerator", type=str, default="cuda", help="Accelerator (cpu, cuda, mps)"
     )
-    parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
+    parser.add_argument("--batch_size", type=int, default=256, help="Batch size")
     parser.add_argument("--log_dir", type=str, default="./", help="Batch size")
     return parser
 
